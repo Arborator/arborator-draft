9a9d3e807b034acea4fccd3e23fdaeaf93ffc1cc
kim gerdes <kim@gerdes.fr>
2019-09-23 11:21:08 +0200

    added snap rework of graphical edition and display

--- arborator-draft.js
+++ arborator-draft.js
@@ -17,11 +17,15 @@
  *
  */
 
-var drag = d3.drag();
+// var drag = d3.drag();
+
+// xWordDistance = 20;
+yWordDistance = 20;
 
 // global variables:
 fontSize = 0; // computed from css value for .token in arborator-draft.css
 lemmaHeight = 0;
+maxLevelDistance = 33; // the maximum distance between one dependency relation and the next higher one
 posHeight = 0;
 svgDefaultHeight = 500;
 el=10; // type of conll (10, 14, or 4), computed in conllNodesToTree
@@ -30,7 +34,7 @@ uextras=[]; // list of comments. each comment is a hashtable position(=line)->co
 conlltrees=[]; // list of conll strings
 defaultCat="_"
 shownfeatures=["t", "cat", "lemma","gloss"]; // recomputed in readConll
-progressiveLoading = true; // false to make it load all trees at once (may overload the browser)
+// progressiveLoading = true; // false to make it load all trees at once (may overload the browser)
 pngBtn = true;
 svgBtn = true;
 reverseMode = false; // set true for right to left conll
@@ -41,6 +45,14 @@ conlls = {
 }
 isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1; // needed for bezier bounding box bug
 
+currentS = null;
+// drag & drop stuff:
+droppables =[];
+dragrepl = null;
+dragcurve = null;
+dragarrowhead = null;
+dragsun = null;
+dragover = -1;
 // debug:
 log = console.log.bind(console);
 
@@ -56,20 +68,20 @@ svgIdIndex = 0;
 // public initialisation function
 this.ArboratorDraft = function(visuMode = 0, reverse = false) {
 	// main function called from html file
-	if(reverse) reverseMode = true;
-	if(visuMode==0){
-		$( ".expander" ).click(function(){
-			log(99,$(this).next('conll'));
-			$(this).next('conll').toggle();});    
-		readConll();
-	}else{
+	// if(reverse) reverseMode = true;
+	// if(visuMode==0){
+	// 	$( ".expander" ).click(function(){
+	// 		log(99,$(this).next('conll'));
+	// 		$(this).next('conll').toggle();});    
+	// 	readConll();
+	// }else{
 		console.log('[ArboratorDraft] visumode');
 		trees=[]; 
 		uextras=[]; 
 		conlltrees=[]; 
 		$('conll').hide();
 		refresh( $('#conllarea').text() );
-	}
+	// }
 }
 
 // public function
@@ -81,12 +93,406 @@ ArboratorDraft.prototype.emptyThenRefresh = function(content, reverse = false, t
 
 
 function refresh(content) {
-	$('#svgwell').html('');
-	$('#svgwell').append( $("<conll></conll>").attr('id', 'transformhere').text( content ) );
-	var conll = d3.selectAll('#transformhere')['_groups'][0][0];
-	drawConll(conll);
+	// $('#svgwell').html('');
+	// $('#svgwell').append( $("<conll></conll>").attr('id', 'transformhere').text( content ) );
+	// var conll = d3.selectAll('#transformhere')['_groups'][0][0];
+	// console.log(content);
+	// drawConll(conll);
+	listOfConlls = content.trim().split(/\n\s*\n\s*\n*/);	
+	
+	for (let singleConll of listOfConlls) { // for each conll tree at once, can block the browser
+		var treedata = conllNodesToTree(singleConll)
+		// console.log("refresh function",treedata);
+		drawsnap($("#svgwell"), treedata, shownfeatures)
+		
+		}
 	return;
 }
+
+function level(i,gi,tree, idgov2level) {
+	// recursive function determining the level of a dependency relation
+	// log("__________level",i,gi)
+	if (idgov2level[i+'_'+gi]>0) return idgov2level[i+'_'+gi];
+	if (gi==0) return 0;
+	var intermlevs = [1];
+	for (var ii = Math.min(i,gi)+1; ii < Math.max(i,gi); ii++) {
+		if (tree[ii])
+			for (var gii in tree[ii]["gov"]) {
+				if (gii>=Math.min(i,gi) && gii<=Math.max(i,gi)) {
+					if (idgov2level[ii+'_'+gii]>0) intermlevs.push(idgov2level[ii+'_'+gii]+1);
+					else intermlevs.push(level(ii, gii, tree, idgov2level)+1);
+
+				}
+				
+			}
+	}
+	idgov2level[i+'_'+gi] = Math.max( ...intermlevs );
+	// log("__________level",i,gi,'returns',idgov2level[i+'_'+gi])
+	return idgov2level[i+'_'+gi];
+}
+
+
+function dragging(dx, dy, posX, posY, event){
+	// log("onmove",posX, posY,this,dx, dy, posX, posY, event,"translate("+posX+","+posY+")")
+	this.transform("translate("+(dx-15)+","+(dy-30)+")").attr({"class":"gloss"});
+	x=this.midx;
+	y=this.topy;
+	var path = "M"+x+","+y+" C"+x+","+(y-Math.abs(dx)/2)+" "+(x+dx)+","+(y-Math.abs(dx)/2)+" "+(x+dx)+","+(y+dy);
+	dragcurve.attr({d:path});
+	dragarrowhead.transform("translate("+dx+","+dy+")")
+	// log(dragsun)
+	if (y+dy<100) {if (dragsun==null) dragsun = this.paper.root.circle(x,0,77).attr("class", "dragcurve")}
+	else {if(dragsun!=null) {dragsun.remove();dragsun=null}}
+}
+
+var startdrag = function(xx,yy,e) {
+	// this.data('origTransform', this.transform().local );
+	dragrepl = this.clone();
+	dragrepl.attr({class:"draggov"});
+	this.attr({cursor: "move"})
+
+	x=this.midx;
+	y=this.topy;
+	// log("-----------", this.attr("x"));
+	var path = "M"+x+","+y+" C"+x+","+(y-1)+" "+(x+1)+","+(y-1)+" "+(x+1)+","+y;
+	// log("path",path)
+	dragcurve = this.paper.root.path(path).attr({class:"dragcurve"});
+	dragarrowhead = this.paper.root.path(arrowhead(x,y)).attr("class", "dragcurve");
+	
+	for (let dropa of droppables) 
+		{ 
+			if (dropa!=this) 
+			{
+				dropa.mouseover(
+					function() {
+						// console.log("Over the word", this.nr);
+						dropa.attr({"class":"gloss"});
+						dragover = this.nr
+					});
+				dropa.mouseout(
+					function() {
+						// console.log("out from the word", this.nr);
+						dropa.attr({"class":"t"});
+						dragover = -1
+					});
+				dropa.attr({cursor: "move"});
+			}
+		}
+		
+}
+
+
+var stopdrag = function(e) {
+	
+	this.animate({transform: "translate("+0+","+0+")"}, 300, mina.easein,
+		function()
+			{
+				this.attr({"class":"t"});
+				dragrepl.remove();
+			}
+		);
+	
+	for (let dropa of droppables) 
+	{ 
+		dropa.unmouseover();
+		dropa.unmouseout();
+		dropa.attr({cursor: "move", cursor: "grab", "class":"t"})
+	}
+	dragcurve.remove();
+	dragarrowhead.remove();
+	
+	if ( (dragover>=0 && this.nr != undefined) || dragsun!=null) 
+	{
+		nr = this.nr;
+		if (dragsun) {dragover = nr; nr=0; }
+		
+		oldFunction = Object.values(this.paper.root.treedata.tree[dragover]['gov'])[0]		
+		newtree = getNewFunction(this.paper.root.treedata.tree, oldFunction, dragover, nr, (e.shiftKey || e.ctrlKey))
+		// log("newtree",newtree)
+		this.paper.root.treedata.tree = newtree;
+		$("#svgwell").html("")
+		refresh( treeDataToConll(this.paper.root.treedata));
+		// $('#conllarea').text(	);
+	}
+	if(dragsun!=null) {dragsun.remove();dragsun=null}
+	dragover=-1;
+}
+
+function getNewFunction(tree, oldFunction, newDep, newGov, add) {
+	log("getNewFunction", oldFunction, newDep, newGov, add)
+	
+	newFunction = oldFunction // TODO: make menu!
+
+	if (add) tree[newDep]['gov'][newGov]=newFunction
+	else {tree[newDep]['gov']={};
+	tree[newDep]['gov'][newGov]=newFunction;
+}
+
+	log("tree[newDep]['gov']",tree[newDep]['gov'])
+	return tree
+}
+
+function drawsnap(div, treedata, shownfeatures) {
+	// draws json tree on svg in div
+	var textstarty = 400;
+	var runningy = textstarty;
+	var s=Snap("#svgwell");
+	s.treedata = treedata;
+	var tree = treedata.tree;
+	// insertion of texts
+	var xpositions = [0];
+	var allstexts = {};
+	
+	var feati = 0;
+	for (let shofea of shownfeatures) { 
+		stexts = [];
+		ind = 0;
+		for (let nr of tree) {
+		// Object.keys(tree).forEach(function(nr) {
+			var word = tree[nr];
+			var sword = s.text(xpositions[ind], runningy, word[shofea]).attr({class:shofea});
+			sword.wordDistance = parseInt(getComputedStyle(sword.node).getPropertyValue('--wordDistance'));
+			sword.nr = nr;
+			
+			stexts.push(sword);
+			if (feati==0) {
+				sword.attr({cursor: "move", cursor: "grab"}).drag( dragging, startdrag, stopdrag); // dragging only the first line (normally the tokens)
+				droppables.push(sword)
+			}
+			
+			nextx = Math.max(xpositions[ind+1] || 0, xpositions[ind]+sword.getBBox().w + sword.wordDistance);
+			xpositions[ind+1]=nextx;
+			ind += 1;
+		};
+		runningy += yWordDistance;
+		allstexts[shofea]=stexts;
+		feati += 1;
+	}
+	var xmidpoints = [];
+	var firstTextFontSize = parseInt(getComputedStyle(allstexts[shownfeatures[0]][0].node).getPropertyValue('font-size'));
+	// readjusting the x position of texts
+	for (let shofea of shownfeatures) { 
+		var ind = 0;
+		Object.keys(tree).forEach(function(nr) {
+			// allstexts[shofea][ind].attr({'x':  xpositions[ind]}); // simple left aligned table
+			var xmidpoint = xpositions[ind]+(xpositions[ind+1]-xpositions[ind])/2; //-allstexts[shofea][ind].wordDistance
+			xmidpoints.push(xmidpoint);
+			allstexts[shofea][ind].midx=xmidpoint;
+			allstexts[shofea][ind].attr({'x':  xmidpoint - allstexts[shofea][ind].getBBox().w/2});
+			allstexts[shofea][ind].topy = allstexts[shofea][ind].getBBox().y;
+			ind += 1;
+		});
+	}
+
+	var ind = 0;
+	
+	var idgov2level = {};
+	var levels = [];
+	// compute the max level
+	Object.keys(tree).forEach(function(nr) { // for each dependent
+		var word = tree[nr];
+		for (var govid in word["gov"]) { // for each governor
+			if (tree[govid]) levels.push(level(nr,govid,tree, idgov2level));
+		}
+	});
+	var maxlevel = Math.max( ...levels);
+	var basey = textstarty-firstTextFontSize-2;
+	var leveldistance = Math.min(basey/maxlevel,maxLevelDistance); // take the min of the available height divided by the required levels and the maxLevelDistance
+	// drawing the dependency relations
+	Object.keys(tree).forEach(function(nr) { // for each dependent
+		var word = tree[nr];
+		for (var govid in word["gov"]) { // for each governor
+			if (govid in tree || govid==0) // only existing governors
+			{
+				le = idgov2level[nr+'_'+govid]; // level(nr, govid, tree, idgov2level);
+				var controly = basey-le*leveldistance;
+				var tox = govid > ind ? xmidpoints[govid-1]-firstTextFontSize/2 : xmidpoints[govid-1]+firstTextFontSize/2;
+				if (govid>0) var path = "M"+xmidpoints[ind]+","+basey+" C"+xmidpoints[ind]+","+controly+" "+tox+","+controly+" "+tox+","+basey;
+				else var path = "M"+xmidpoints[ind]+","+(textstarty-firstTextFontSize-2)+" L"+xmidpoints[ind]+","+"0 ";
+				var p = s.path(path).attr({class:"curve"});
+				s.path(arrowhead(xmidpoints[ind],textstarty-firstTextFontSize-2)).attr("class", "arrowhead");
+				var pbox = p.getBBox();
+				var xmidf = pbox.x+(pbox.w)/2;
+				var srel = s.text(xmidf, pbox.y-2, word["gov"][govid]).attr({class:"deprel"});
+				if (govid==0) srel.attr({'y':  firstTextFontSize})
+				else srel.attr({'x':  xmidf - srel.getBBox().w/2});
+			}
+		}
+		ind += 1;
+	});
+
+}
+
+function arrowhead(x,y) {
+	// gives path for arrowhead x,y startpoint (end of arrow)
+	var size = 5;
+	var startpoint = x+","+y; // to move the arrowhead lower: (y+size/3);
+	var lefttop = "0,0" +(-size/2)+","+(-size*1.5)+" "+(-size/2)+","+(-size*1.5);
+	var righttop = (size/2)+"," +(size/2)+" "+(size/2)+"," +(size/2)+ " "+(size)+",0";
+	var arrowPath = "M"+ startpoint+"c"+lefttop+ "c"+righttop+ "z";
+	return arrowPath;
+}
+
+
+
+
+function treeDataToConll(treedata)
+{
+	// log(treedata.uextra[0])
+	conllstrs=treedata.uextra[0]
+	for (let nr in treedata.tree){
+		var node = treedata.tree[nr];
+		// log('_______________',node)
+		govs=node["gov"]||{};
+		govk = Object.keys(govs).sort();
+		if (govk) {
+			gk = govk[0];
+			gv = govs[gk] || "_"
+		}
+		else {
+			gk = '_';
+			gv = '_';
+		}
+		featstr = "";
+		for (let a in node){
+			if (!(["t","lemma","cat","tag","tag2","xpos","egov","misc","id","index","gov","kids","govrel"].includes(a))){
+				featstr += a+"="+node[a];
+			}
+		}
+		othergovstr = "";
+		for (let g in govk.slice(1)){
+				othergovstr += g+":"+govs[g];
+		}
+		conllstrs.push(
+			[
+				nr,
+				(node['t']||'_'),
+				(node['lemma']||'_'),
+				(node['cat']||'_'),
+				(node['xpos']||'_'),
+				(node['morph']||'_'),
+				gk,
+				gv,
+				(othergovstr||'_'),
+				(node['gloss']||'_') // NoSpaceAfter???
+			].join("\t"))
+	};
+	conllstr=conllstrs.join("\n")
+	return conllstr
+
+
+}
+
+function conllNodesToTree(treeline) {
+	// reads a conll representation of a single tree TODO: replace jquery by d3
+	
+	var nodes = treeline.split('\n');
+	if(reverseMode)nodes.reverse();
+	// nodes = nodes.reverse();
+	var tree={};
+	var uextra={};
+	var lastid=0;
+	var skipuntil=0;
+	var words=[]
+	$.each(nodes, function(id,nodeline){ // for each conll line:
+		var nodeline=$.trim(nodeline);
+		if (nodeline.charAt(0) == "#") { 
+			if (!(lastid in uextra)) uextra[lastid]=[]; // why why why
+			uextra[lastid].push(nodeline)
+			return true;
+		}
+		var elements = nodeline.split('\t');
+		el=elements.length;
+
+		if (!(el in conlls) && el>10) el=10;
+		if (el > 4) id=elements[conlls[el]["id"]];
+		else if (elements[conlls[el]["t"]] != "_") id++;
+		if (lastid!=id) // needed for the arborator encoding of multiple govs
+		{
+			var t=elements[conlls[el]["t"]];
+			var tokids=id.split("-")
+			if (tokids.length == 1) {
+				tree[id]={}
+				tree[id]["gov"]={};
+				tree[id]["t"]=t;
+				tree[id]["id"]=id;
+				tree[id]["lemma"]=elements[conlls[el]["lemma"]];
+				tree[id]["cat"]=elements[conlls[el]["cat"]];
+				if (id>skipuntil) words.push(t);
+				if (el==10) {
+					tree[id]["xpos"]=elements[conlls[el]["xpos"]];
+					tree[id]["morph"]=elements[conlls[el]["morph"]];
+					tree[id]["gloss"]=elements[conlls[el]["gloss"]];
+					if (tree[id]["gloss"]=="SpaceAfter=No"){
+						tree[id]["gloss"]="_";
+						tree[id]["NoSpaceAfter"]=false;
+					}
+					var xgov = elements[conlls[el]["xgov"]];
+					if (xgov.indexOf(':') > -1){
+						var xgovs=xgov.split("|");
+						$.each(xgovs, function(ind,xg){ 
+							// for each extra governor line:
+							var xgs=xg.split(":")
+							if (xgs.length >=2) {
+								// if it's not just _
+								var gov=xgs[0];
+								var func= xgs.slice(1).join(":");
+								tree[id]["gov"][gov]=func;
+							}
+						});
+					}
+				}
+			}
+			else if (tokids.length == 2){
+				skipuntil = parseInt(tokids[1])
+				words.push(elements[conlls[el]["t"]]);
+				if (!(lastid in uextra)) uextra[lastid]=[];
+				uextra[lastid].push(nodeline)
+			}
+			else {
+				if (!(lastid in uextra)) uextra[lastid]=[];
+				uextra[lastid].push(nodeline)
+			}
+		}
+		gov = elements[conlls[el]["gov"]];
+		if (gov!="" && gov!="_") 
+		{
+			if (gov==-1)
+			{
+				gov = elements[conlls[el]["gov"]+1];
+			}
+			var func = elements[conlls[el]["func"]];
+			if (func.indexOf('::') !== -1) 
+				{	
+					var stydic = func.substring(func.indexOf("::") + 1);
+					func = func.split("::")[0];
+					if (stydic!="") funcDic[func] = $.parseJSON(stydic);
+					$('#styleconllcheck').prop('checked', true);
+				};
+			tree[id]["gov"][gov]=func;
+			
+		}
+		lastid=id;
+		});
+	
+	
+	var sentence="";
+	words.forEach(function (word, i) {
+		sentence+=word;
+		if(!reverseMode){
+			if (i+1 in tree && !(("NoSpaceAfter" in tree[i+1]) && tree[i+1]["NoSpaceAfter"]==false)) sentence+=" ";
+		}else{
+			sentence+=" ";
+		}
+	});
+	return {tree:tree, uextra:uextra, sentence:sentence};
+}
+
+///////////////////////
+
+
+
+
 
 function sleep(milliseconds) {
 	var start = new Date().getTime();
@@ -100,7 +506,7 @@ function sleep(milliseconds) {
 function empty() {
 	console.log("empty");
 	var def = new jQuery.Deferred();
-	clearTimeout(readInsideConllTimeout);
+	// clearTimeout(readInsideConllTimeout);
 	treelines = [];
 	$('#svgwell').html('');
 	def.resolve();
@@ -125,6 +531,9 @@ function progressiveReadConll() {
 
 	waitBetweenElements(conllLoop.length);
 }
+
+/////////////////////
+
 
 function progressiveReadInsideConll(trees, pnode) {
 	// draw each tree INSIDE a conll progressively
@@ -243,111 +652,6 @@ function drawConll(conllElement) { // for each <conll> section:
 }
 
 
-function conllNodesToTree(treeline) {
-	// reads a conll representation of a single tree TODO: replace jquery by d3
-	
-	var nodes = treeline.split('\n');
-	if(reverseMode)nodes.reverse();
-	// nodes = nodes.reverse();
-	var tree={};
-	var uextra={};
-	var lastid=0;
-	var skipuntil=0;
-	var words=[]
-	$.each(nodes, function(id,nodeline){ // for each conll line:
-		var nodeline=$.trim(nodeline);
-		if (nodeline.charAt(0) == "#") {
-			if (!(lastid in uextra)) uextra[lastid]=[];
-			uextra[lastid].push(nodeline)
-			return true;
-		}
-		var elements = nodeline.split('\t');
-		el=elements.length;
-
-		if (!(el in conlls) && el>10) el=10;
-		if (el > 4) id=elements[conlls[el]["id"]];
-		else if (elements[conlls[el]["t"]] != "_") id++;
-		if (lastid!=id) // needed for the arborator encoding of multiple govs
-		{
-			var t=elements[conlls[el]["t"]];
-			var tokids=id.split("-")
-			if (tokids.length == 1) {
-				tree[id]={}
-				tree[id]["gov"]={};
-				tree[id]["t"]=t;
-				tree[id]["id"]=id;
-				tree[id]["lemma"]=elements[conlls[el]["lemma"]];
-				tree[id]["cat"]=elements[conlls[el]["cat"]];
-				if (id>skipuntil) words.push(t);
-				if (el==10) {
-					tree[id]["xpos"]=elements[conlls[el]["xpos"]];
-					tree[id]["morph"]=elements[conlls[el]["morph"]];
-					tree[id]["gloss"]=elements[conlls[el]["gloss"]];
-					if (tree[id]["gloss"]=="SpaceAfter=No"){
-						tree[id]["gloss"]="_";
-						tree[id]["NoSpaceAfter"]=false;
-					}
-					var xgov = elements[conlls[el]["xgov"]];
-					if (xgov.indexOf(':') > -1){
-						var xgovs=xgov.split("|");
-						$.each(xgovs, function(ind,xg){ 
-							// for each extra governor line:
-							var xgs=xg.split(":")
-							if (xgs.length >=2) {
-								// if it's not just _
-								var gov=xgs[0];
-								var func= xgs.slice(1).join(":");
-								tree[id]["gov"][gov]=func;
-							}
-						});
-					}
-				}
-			}
-			else if (tokids.length == 2){
-				skipuntil = parseInt(tokids[1])
-				words.push(elements[conlls[el]["t"]]);
-				if (!(lastid in uextra)) uextra[lastid]=[];
-				uextra[lastid].push(nodeline)
-			}
-			else {
-				if (!(lastid in uextra)) uextra[lastid]=[];
-				uextra[lastid].push(nodeline)
-			}
-		}
-		gov = elements[conlls[el]["gov"]];
-		if (gov!="" && gov!="_") 
-		{
-			if (gov==-1)
-			{
-				gov = elements[conlls[el]["gov"]+1];
-			}
-			var func = elements[conlls[el]["func"]];
-			if (func.indexOf('::') !== -1) 
-				{	
-					var stydic = func.substring(func.indexOf("::") + 1);
-					func = func.split("::")[0];
-					if (stydic!="") funcDic[func] = $.parseJSON(stydic);
-					$('#styleconllcheck').prop('checked', true);
-				};
-			tree[id]["gov"][gov]=func;
-			
-		}
-		lastid=id;
-		});
-	
-	
-	var sentence="";
-	words.forEach(function (word, i) {
-		sentence+=word;
-		if(!reverseMode){
-			if (i+1 in tree && !(("NoSpaceAfter" in tree[i+1]) && tree[i+1]["NoSpaceAfter"]==false)) sentence+=" ";
-		}else{
-			sentence+=" ";
-		}
-	});
-	return {tree:tree, uextra:uextra, sentence:sentence};
-}
-
 
 function getSVGPath(startPoint,endPoint,computedStyle) {
 	// startPoint and endPoint are objects for the corresponding nodes
@@ -372,15 +676,6 @@ function getSVGPath(startPoint,endPoint,computedStyle) {
 }
 
 
-function arrowhead(x,y) {
-	// gives path for arrowhead x,y startpoint (end of arrow)
-	var size = 5;
-	var startpoint = x+","+y; // to move the arrowhead lower: (y+size/3);
-	var lefttop = "0,0" +(-size/2)+","+(-size*1.5)+" "+(-size/2)+","+(-size*1.5);
-	var righttop = (size/2)+"," +(size/2)+" "+(size/2)+"," +(size/2)+ " "+(size)+",0";
-	var arrowPath = "M"+ startpoint+"c"+lefttop+ "c"+righttop+ "z";
-	return arrowPath;
-}
 
 
 function draw(div, tree) {
